3a89d28f7f6939b28785e8c76ef970fbb376a966
saleh Al saleh <saleh.alsaleh802@gmail.com>
2024-03-17 22:27:37 +0300

    finished sign out feature

--- lib/screens/Home/admin/HomeAdmin.dart
+++ lib/screens/Home/admin/HomeAdmin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:safeer/models/user.dart';
 import 'package:safeer/services/auth.dart';
 
 class HomeAdmin extends StatefulWidget {
@@ -17,7 +19,9 @@ class _HomeAdminState extends State<HomeAdmin> {
         TextButton(
             onPressed: () async {
               final result = await _auth.signOut();
+
               if (result == null) {
+                context.read<UserProvider>().updateUid(result);
                 print("signed out");
               } else {
                 print(result.toString());
